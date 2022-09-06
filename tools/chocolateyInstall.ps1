753adfc80db5891602b205193fe8a0eb40023964
devblok <l.praninskas@gmail.com>
2022-09-06 21:36:30 +0300

    Bump to v1.2.12

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ if (-not ($packageParameters)) {
 }
 
 # Nomad related variables
-$nomadVersion = '1.1.18'
+$nomadVersion = '1.2.12'
 $sourcePath = if (Get-ProcessorBits 32) {
   $(Join-Path $binariesPath "$($nomadVersion)_windows_386.zip")
 } else {
